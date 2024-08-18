39f4e17145e725e243294ab6cc5261199231863d
FelipePerez99 <andresfppy@gmail.com>
2024-08-18 14:59:17 -0500

    Agregar BeanConfigurations con su respectivo método

---
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/configuration/BeanConfigurations.java
@@ -0,0 +1,17 @@
+package com.bootcamp.microservicio_stock.infrastructure.configuration;
+
+
+import com.bootcamp.microservicio_stock.application.output.FormatterResultsIntPort;
+import com.bootcamp.microservicio_stock.application.output.ManageCategoryGatewayIntPort;
+import com.bootcamp.microservicio_stock.domain.useCases.ManageCategoryCUAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfigurations {
+    @Bean
+    public ManageCategoryCUAdapter createManageCategoryCUInt(ManageCategoryGatewayIntPort objManageCategoryGateway, FormatterResultsIntPort objFormatterResults) {
+        ManageCategoryCUAdapter objManageCategoryCU= new ManageCategoryCUAdapter(objFormatterResults);
+        return objManageCategoryCU;
+    }
+}
