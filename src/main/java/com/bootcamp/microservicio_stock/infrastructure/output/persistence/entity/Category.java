639f759da8e3a97a84049ad3c3b6c6eb705f1eb7
FelipePerez99 <andresfppy@gmail.com>
2024-08-18 12:28:17 -0500

    Agregar entidad Category con sus validaciones

---
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/entity/Category.java
@@ -0,0 +1,25 @@
+package com.bootcamp.microservicio_stock.infrastructure.output.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 90)
+    private String description;
+}
