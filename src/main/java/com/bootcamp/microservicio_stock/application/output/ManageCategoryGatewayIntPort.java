32a18a61247e04c1abe6f13ec378354aefc6f55b
FelipePerez99 <andresfppy@gmail.com>
2024-08-18 12:09:37 -0500

    Agregar clase Category

--- src/main/java/com/bootcamp/microservicio_stock/application/output/ManageCategoryGatewayIntPort.java
+++ src/main/java/com/bootcamp/microservicio_stock/application/output/ManageCategoryGatewayIntPort.java
@@ -1,5 +1,7 @@
 package com.bootcamp.microservicio_stock.application.output;
 
+import com.bootcamp.microservicio_stock.domain.models.Category;
+
 public interface ManageCategoryGatewayIntPort {
     public Category save(Category objCategory);
 }
---
+++ src/main/java/com/bootcamp/microservicio_stock/domain/models/Category.java
@@ -0,0 +1,16 @@
+package com.bootcamp.microservicio_stock.domain.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    private int id;
+    private String name;
+    private String description;
+}
