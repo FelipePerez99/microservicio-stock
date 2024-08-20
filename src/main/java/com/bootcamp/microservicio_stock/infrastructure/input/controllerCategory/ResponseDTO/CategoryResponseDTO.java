e09048c99c598295b177d1fd5ba47915ee51a70e
FelipePerez99 <andresfppy@gmail.com>
2024-08-20 14:57:33 -0500

    Agregar CategoryResponseDTO con sus respectivos atributos

--- src/main/java/com/bootcamp/microservicio_stock/infrastructure/input/controllerCategory/ResponseDTO/CategoryResponseDTO.java
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/input/controllerCategory/ResponseDTO/CategoryResponseDTO.java
@@ -1,4 +1,16 @@
 package com.bootcamp.microservicio_stock.infrastructure.input.controllerCategory.ResponseDTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryResponseDTO {
+    private int id;
+    private String name;
+    private String description;
 }
