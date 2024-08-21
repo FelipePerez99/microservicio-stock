b53b03d8a897c87077a0e2981575abaa6de8189b
FelipePerez99 <andresfppy@gmail.com>
2024-08-21 09:28:53 -0500

    Correcion a CategoryMapper

--- src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/mapper/CategoryMapper.java
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/mapper/CategoryMapper.java
@@ -1,12 +1,13 @@
 package com.bootcamp.microservicio_stock.infrastructure.output.persistence.mapper;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CategoryMapper {
     @Bean
-    public CategoryMapper categoryMapper() {
-        return new CategoryMapper();
+    public ModelMapper categoryMapper() {
+        return new ModelMapper();
     }
 }
