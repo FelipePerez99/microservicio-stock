8058122e7fba2fd8f7af69c4bd116203d57ad8bc
FelipePerez99 <andresfppy@gmail.com>
2024-08-21 09:29:53 -0500

    Correcion a CategoryRepositoryInt

--- src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/repository/CategoryRepositoryInt.java
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/repository/CategoryRepositoryInt.java
@@ -1,7 +1,8 @@
 package com.bootcamp.microservicio_stock.infrastructure.output.persistence.repository;
 
+import com.bootcamp.microservicio_stock.infrastructure.output.persistence.entity.CategoryEntity;
 import jakarta.persistence.Cache;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepositoryInt extends JpaRepository<CategoryRepositoryInt, Integer>{
+public interface CategoryRepositoryInt extends JpaRepository<CategoryEntity, Integer>{
 }
