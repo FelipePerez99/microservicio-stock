edb01860137a2f9223f4848247fad96adfe45803
FelipePerez99 <andresfppy@gmail.com>
2024-08-21 09:27:25 -0500

    Agregar implementación al método save

--- src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/gateway/ManageCategoryGatewayImplAdapter.java
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/gateway/ManageCategoryGatewayImplAdapter.java
@@ -2,13 +2,27 @@ package com.bootcamp.microservicio_stock.infrastructure.output.persistence.gatew
 
 import com.bootcamp.microservicio_stock.application.output.ManageCategoryGatewayIntPort;
 import com.bootcamp.microservicio_stock.domain.models.Category;
+import com.bootcamp.microservicio_stock.infrastructure.output.persistence.entity.CategoryEntity;
+import com.bootcamp.microservicio_stock.infrastructure.output.persistence.repository.CategoryRepositoryInt;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.modelmapper.TypeToken;
 
+@RequiredArgsConstructor
 @Service
 public class ManageCategoryGatewayImplAdapter implements ManageCategoryGatewayIntPort {
+
+    private final CategoryRepositoryInt objCategoryRepository;
+    private final ModelMapper categoryModelMapper;
+
     @Override
     public Category save(Category objCategory) {
-        return null;
+        CategoryEntity objCategoryEntity = this.categoryModelMapper.map(objCategory, CategoryEntity.class);
+        Category objCategoryResponse = null;
+        CategoryEntity objCategoryEntityRegistered = this.objCategoryRepository.save(objCategoryEntity);
+        objCategoryResponse = this.categoryModelMapper.map(objCategoryEntityRegistered, Category.class);
+        return objCategoryResponse;
     }
 
     @Override
