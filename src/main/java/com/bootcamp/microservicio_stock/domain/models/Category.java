55f687b93252c19e6ec50dd5a04cb10115a397f9
FelipePerez99 <andresfppy@gmail.com>
2024-08-22 09:37:38 -0500

    Modificar Category implementando constructo, getter, setter y quitando anotaciones

--- src/main/java/com/bootcamp/microservicio_stock/domain/models/Category.java
+++ src/main/java/com/bootcamp/microservicio_stock/domain/models/Category.java
@@ -1,16 +1,42 @@
 package com.bootcamp.microservicio_stock.domain.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Category {
     private int id;
     private String name;
     private String description;
+
+    public Category() {
+    }
+
+    public Category(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
