59afa8a34837a702f0c053d2a081772e12bfad60
FelipePerez99 <andresfppy@gmail.com>
2024-08-22 09:49:12 -0500

    Agregar metodo existsCategoryByName

--- src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/repository/CategoryRepositoryInt.java
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/repository/CategoryRepositoryInt.java
@@ -3,6 +3,9 @@ package com.bootcamp.microservicio_stock.infrastructure.output.persistence.repos
 import com.bootcamp.microservicio_stock.infrastructure.output.persistence.entity.CategoryEntity;
 import jakarta.persistence.Cache;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CategoryRepositoryInt extends JpaRepository<CategoryEntity, Integer>{
+    @Query("SELECT count(*) FROM CategoryEntity c  WHERE c.name=?1")
+    Integer existsCategoryByName(String name);
 }
