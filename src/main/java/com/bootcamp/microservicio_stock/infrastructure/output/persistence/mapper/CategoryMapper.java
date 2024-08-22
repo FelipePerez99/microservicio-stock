f559505cf9b9fdb38c4c508e1a6cbdfdc977b4b7
FelipePerez99 <andresfppy@gmail.com>
2024-08-21 22:27:30 -0500

    Modificar método mapperCategory en CategoryMapper

--- src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/mapper/CategoryMapper.java
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/mapper/CategoryMapper.java
@@ -1,5 +1,6 @@
 package com.bootcamp.microservicio_stock.infrastructure.output.persistence.mapper;
 
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CategoryMapper {
     @Bean
-    public ModelMapper categoryMapper() {
+    public ModelMapper mapperCategory() {
         return new ModelMapper();
     }
 }
