54fa1a0786770c45793d9cb19b421a3e568ca9e7
FelipePerez99 <andresfppy@gmail.com>
2024-08-22 09:39:20 -0500

    Modificar CategoryRequestDTO implementando constructo, getter, setter y quitando anotaciones

--- src/main/java/com/bootcamp/microservicio_stock/infrastructure/input/controllerCategory/RequestDTO/CategoryRequestDTO.java
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/input/controllerCategory/RequestDTO/CategoryRequestDTO.java
@@ -2,15 +2,7 @@ package com.bootcamp.microservicio_stock.infrastructure.input.controllerCategory
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class CategoryRequestDTO {
     private int id;
 
@@ -18,6 +10,40 @@ public class CategoryRequestDTO {
     @Size(min = 3 , max = 50)
     private String name;
 
+    @NotNull(message = "La descripción no puede estar vacía")
     @Size(min = 5 , max = 90)
     private String description;
+
+    public CategoryRequestDTO() {
+    }
+
+    public CategoryRequestDTO(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public @NotNull(message = "El nombre no puede estar vacio") @Size(min = 3, max = 50) String getName() {
+        return name;
+    }
+
+    public void setName(@NotNull(message = "El nombre no puede estar vacio") @Size(min = 3, max = 50) String name) {
+        this.name = name;
+    }
+
+    public @NotNull(message = "La descripción no puede estar vacía") @Size(min = 5, max = 90) String getDescription() {
+        return description;
+    }
+
+    public void setDescription(@NotNull(message = "La descripción no puede estar vacía") @Size(min = 5, max = 90) String description) {
+        this.description = description;
+    }
 }
