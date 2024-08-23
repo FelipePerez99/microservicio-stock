30352e2cb0506d5fddb8249a014bb0f8f3561cdd
FelipePerez99 <andresfppy@gmail.com>
2024-08-23 16:35:16 -0500

    feat: Agregar metodo listCategories a la interfaz ManageCategoryCUIntPort

--- src/main/java/com/bootcamp/microservicio_stock/application/input/ManageCategoryCUIntPort.java
+++ src/main/java/com/bootcamp/microservicio_stock/application/input/ManageCategoryCUIntPort.java
@@ -1,7 +1,10 @@
 package com.bootcamp.microservicio_stock.application.input;
 
 import com.bootcamp.microservicio_stock.domain.models.Category;
+import org.springframework.data.domain.Page;
+import java.awt.print.Pageable;
 
 public interface ManageCategoryCUIntPort {
     public Category create(Category objCategory);
+    public Page<Category> listCategories(Pageable pageable);
 }
