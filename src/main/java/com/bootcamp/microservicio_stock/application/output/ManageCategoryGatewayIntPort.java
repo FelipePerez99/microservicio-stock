44aae9a4b303fc2e1e388eb9c33e947d746ee553
FelipePerez99 <andresfppy@gmail.com>
2024-08-23 16:52:40 -0500

    feat: Agregar metodo findAll a la interfaz ManageCategoryGatewayIntPort

--- src/main/java/com/bootcamp/microservicio_stock/application/output/ManageCategoryGatewayIntPort.java
+++ src/main/java/com/bootcamp/microservicio_stock/application/output/ManageCategoryGatewayIntPort.java
@@ -1,8 +1,12 @@
 package com.bootcamp.microservicio_stock.application.output;
 
 import com.bootcamp.microservicio_stock.domain.models.Category;
+import org.springframework.data.domain.Page;
+
+import java.awt.print.Pageable;
 
 public interface ManageCategoryGatewayIntPort {
     public Category save(Category objCategory);
     public boolean existsCategoryByName(String name);
+    Page<Category> findAll(Pageable pageable);
 }
