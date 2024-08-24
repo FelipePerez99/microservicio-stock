0888f74d01243035bc4f6e4aeba8c8bf66be1741
FelipePerez99 <andresfppy@gmail.com>
2024-08-23 19:38:35 -0500

    fix:Corregir parametros en metodo listCategories en ManageCategoryCUIntPort

--- src/main/java/com/bootcamp/microservicio_stock/application/input/ManageCategoryCUIntPort.java
+++ src/main/java/com/bootcamp/microservicio_stock/application/input/ManageCategoryCUIntPort.java
@@ -2,9 +2,10 @@ package com.bootcamp.microservicio_stock.application.input;
 
 import com.bootcamp.microservicio_stock.domain.models.Category;
 import org.springframework.data.domain.Page;
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
+
 
 public interface ManageCategoryCUIntPort {
     public Category create(Category objCategory);
-    public Page<Category> listCategories(Pageable pageable);
+    Page<Category> listCategories(int page, int size, String sortBy, boolean ascending);
 }
