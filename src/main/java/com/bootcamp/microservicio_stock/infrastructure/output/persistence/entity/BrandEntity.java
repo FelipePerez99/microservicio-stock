47894438381f2fe3a218eba1986072d005ae2bf2
FelipePerez99 <andresfppy@gmail.com>
2024-08-24 10:02:12 -0500

    feat: Agregar la clase BrandEntity

---
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/entity/BrandEntity.java
@@ -0,0 +1,44 @@
+package com.bootcamp.microservicio_stock.infrastructure.output.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "brands")
+public class BrandEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 120)
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
