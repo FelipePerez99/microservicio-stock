04658f69b363351bcd6d08c46ddc6f6e11709e61
FelipePerez99 <andresfppy@gmail.com>
2024-08-24 10:12:37 -0500

    feat: Agregar la interfaz BrandRepositoryInt con el metodo existsBrandByName

--- src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/repository/BrandRepositoryInt.java
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/repository/BrandRepositoryInt.java
@@ -1,4 +1,11 @@
 package com.bootcamp.microservicio_stock.infrastructure.output.persistence.repository;
 
-public interface BrandRepositoryInt {
+
+import com.bootcamp.microservicio_stock.infrastructure.output.persistence.entity.BrandEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface BrandRepositoryInt extends JpaRepository<BrandEntity, Integer> {
+    @Query("SELECT count(*) FROM BrandEntity b  WHERE b.name=?1")
+    Integer existsBrandByName(String name);
 }
