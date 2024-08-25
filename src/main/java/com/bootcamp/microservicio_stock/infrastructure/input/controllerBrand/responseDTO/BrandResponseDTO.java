110d2732dde606cf11b8ef08eb4c5e6011883c1d
FelipePerez99 <andresfppy@gmail.com>
2024-08-25 15:12:36 -0500

    feat: crear DTO de respuesta para la entidad Brand

--- src/main/java/com/bootcamp/microservicio_stock/infrastructure/input/controllerBrand/responseDTO/BrandResponseDTO.java
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/input/controllerBrand/responseDTO/BrandResponseDTO.java
@@ -1,4 +1,16 @@
 package com.bootcamp.microservicio_stock.infrastructure.input.controllerBrand.responseDTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BrandResponseDTO {
+    private int id;
+    private String name;
+    private String description;
 }
