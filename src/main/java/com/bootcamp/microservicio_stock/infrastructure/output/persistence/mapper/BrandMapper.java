5da777f76595071c6722150dfcc516fa4fd5fa9e
FelipePerez99 <andresfppy@gmail.com>
2024-08-25 15:09:46 -0500

    refactor: agregar @Qualifier al bean ModelMapper en BrandMapper

--- src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/mapper/BrandMapper.java
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/mapper/BrandMapper.java
@@ -1,12 +1,14 @@
 package com.bootcamp.microservicio_stock.infrastructure.output.persistence.mapper;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BrandMapper {
     @Bean
+    @Qualifier("brandMapper")
     public ModelMapper mapperBrand() {
         return new ModelMapper();
     }
