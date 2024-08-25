c2acbd3a0032057028d69571e67819e5b5feec45
FelipePerez99 <andresfppy@gmail.com>
2024-08-25 15:07:49 -0500

    feat: Agregar implementación de ManageBrandGatewayImplAdapter

--- src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/gateway/ManageBrandGatewayImplAdapter.java
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/output/persistence/gateway/ManageBrandGatewayImplAdapter.java
@@ -1,4 +1,37 @@
 package com.bootcamp.microservicio_stock.infrastructure.output.persistence.gateway;
 
-public class ManageBrandGatewayImplAdapter {
+import com.bootcamp.microservicio_stock.application.output.ManageBrandGatewayIntPort;
+import com.bootcamp.microservicio_stock.domain.models.Brand;
+import com.bootcamp.microservicio_stock.infrastructure.output.persistence.entity.BrandEntity;
+import com.bootcamp.microservicio_stock.infrastructure.output.persistence.repository.BrandRepositoryInt;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ManageBrandGatewayImplAdapter implements ManageBrandGatewayIntPort {
+
+    private final BrandRepositoryInt objBrandRepository;
+    private final ModelMapper brandModelMapper;
+
+    public ManageBrandGatewayImplAdapter(BrandRepositoryInt objBrandRepository, @Qualifier("brandMapper") ModelMapper brandModelMapper) {
+        this.objBrandRepository = objBrandRepository;
+        this.brandModelMapper = brandModelMapper;
+    }
+
+    @Transactional
+    @Override
+    public Brand save(Brand objBrand) {
+        BrandEntity objBrandEntity = this.brandModelMapper.map(objBrand, BrandEntity.class);
+        Brand objBrandResponse = null;
+        BrandEntity objBrandEntityRegistered = this.objBrandRepository.save(objBrandEntity);
+        objBrandResponse = this.brandModelMapper.map(objBrandEntityRegistered, Brand.class);
+        return objBrandResponse;
+    }
+
+    @Override
+    public boolean existsBrandyByName(String name) {
+        return this.objBrandRepository.existsBrandByName(name) == 1;
+    }
 }
