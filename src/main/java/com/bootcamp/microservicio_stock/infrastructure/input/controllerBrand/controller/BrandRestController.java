ec6bd3a6136cc5ba658b791024afe2ebfed133c1
FelipePerez99 <andresfppy@gmail.com>
2024-08-25 16:04:44 -0500

    docs: agregar documentación de códigos de respuesta HTTP en el endpoint de creación de marcas

--- src/main/java/com/bootcamp/microservicio_stock/infrastructure/input/controllerBrand/controller/BrandRestController.java
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/input/controllerBrand/controller/BrandRestController.java
@@ -5,6 +5,12 @@ import com.bootcamp.microservicio_stock.domain.models.Brand;
 import com.bootcamp.microservicio_stock.infrastructure.input.controllerBrand.mapper.BrandMapperInfrastructureDomain;
 import com.bootcamp.microservicio_stock.infrastructure.input.controllerBrand.requestDTO.BrandRequestDTO;
 import com.bootcamp.microservicio_stock.infrastructure.input.controllerBrand.responseDTO.BrandResponseDTO;
+import com.bootcamp.microservicio_stock.infrastructure.input.controllerCategory.ResponseDTO.CategoryResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +31,14 @@ public class BrandRestController {
         this.objManageBrandCUInt = objManageBrandCUInt;
         this.objBrandMapper = objBrandMapper;
     }
+
+    @Operation(summary = "Create a new brand", description = "Allows the administrator to create a new brand")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Brand created successfully" ,
+                    content = @Content(schema = @Schema(implementation = BrandResponseDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
 
     @PostMapping("/brand")
     public ResponseEntity<BrandResponseDTO> create(@Valid @RequestBody BrandRequestDTO objBrand){
