c3db4019931d00d34d09e4c179ab4840f871f701
FelipePerez99 <andresfppy@gmail.com>
2024-08-25 15:20:46 -0500

    feat: agregar configuracion de bean para ManageBrandCUAdapter

--- src/main/java/com/bootcamp/microservicio_stock/infrastructure/configuration/BeanConfigurations.java
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/configuration/BeanConfigurations.java
@@ -2,7 +2,9 @@ package com.bootcamp.microservicio_stock.infrastructure.configuration;
 
 
 import com.bootcamp.microservicio_stock.application.output.FormatterResultsIntPort;
+import com.bootcamp.microservicio_stock.application.output.ManageBrandGatewayIntPort;
 import com.bootcamp.microservicio_stock.application.output.ManageCategoryGatewayIntPort;
+import com.bootcamp.microservicio_stock.domain.useCases.ManageBrandCUAdapter;
 import com.bootcamp.microservicio_stock.domain.useCases.ManageCategoryCUAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +15,11 @@ public class BeanConfigurations {
     public ManageCategoryCUAdapter createManageCategoryCUInt(ManageCategoryGatewayIntPort objManageCategoryGateway, FormatterResultsIntPort objFormatterResults) {
         ManageCategoryCUAdapter objManageCategoryCU = new ManageCategoryCUAdapter(objManageCategoryGateway, objFormatterResults);
         return objManageCategoryCU;
+    }
+
+    @Bean
+    public ManageBrandCUAdapter createManageBrandCUInt(ManageBrandGatewayIntPort objManageBrandGateway, FormatterResultsIntPort objFormatterResults) {
+        ManageBrandCUAdapter objManageBrandCU = new ManageBrandCUAdapter(objManageBrandGateway, objFormatterResults);
+        return objManageBrandCU;
     }
 }
