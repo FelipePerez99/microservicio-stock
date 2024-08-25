0364451f5a663f71f7e39f51b8baab6564f5187f
FelipePerez99 <andresfppy@gmail.com>
2024-08-25 15:15:47 -0500

    feat: crear DTO de peticion para la entidad Brand

--- src/main/java/com/bootcamp/microservicio_stock/infrastructure/input/controllerBrand/requestDTO/BrandRequestDTO.java
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/input/controllerBrand/requestDTO/BrandRequestDTO.java
@@ -1,4 +1,24 @@
 package com.bootcamp.microservicio_stock.infrastructure.input.controllerBrand.requestDTO;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class BrandRequestDTO {
+    private int id;
+
+    @NotNull(message = "El nombre no puede estar vacio")
+    @Size(min = 3 , max = 50)
+    private String name;
+
+    @NotNull(message = "La descripción no puede estar vacía")
+    @Size(min = 5 , max = 120)
+    private String description;
 }
