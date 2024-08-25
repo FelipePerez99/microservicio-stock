d9e5a1aaf6c366188a2a7d63450e6b43574ff31d
FelipePerez99 <andresfppy@gmail.com>
2024-08-25 15:17:25 -0500

    feat: agregar mapper para conversión entre Brand y DTOs

--- src/main/java/com/bootcamp/microservicio_stock/infrastructure/input/controllerBrand/mapper/BrandMapperInfrastructureDomain.java
+++ src/main/java/com/bootcamp/microservicio_stock/infrastructure/input/controllerBrand/mapper/BrandMapperInfrastructureDomain.java
@@ -1,4 +1,12 @@
 package com.bootcamp.microservicio_stock.infrastructure.input.controllerBrand.mapper;
 
+import com.bootcamp.microservicio_stock.domain.models.Brand;
+import com.bootcamp.microservicio_stock.infrastructure.input.controllerBrand.requestDTO.BrandRequestDTO;
+import com.bootcamp.microservicio_stock.infrastructure.input.controllerBrand.responseDTO.BrandResponseDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface BrandMapperInfrastructureDomain {
+    Brand mapRequestBrand(BrandRequestDTO request);
+    BrandResponseDTO mapBrandResponse(Brand objBrand);
 }
