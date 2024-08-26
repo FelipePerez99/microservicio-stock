b6f1367df0917304ae45756c01dd1e41261e3f79
FelipePerez99 <andresfppy@gmail.com>
2024-08-26 14:22:39 -0500

    feat: agregar metodo findAllBrands a ManageBrandGatewayIntPort

--- src/main/java/com/bootcamp/microservicio_stock/application/output/ManageBrandGatewayIntPort.java
+++ src/main/java/com/bootcamp/microservicio_stock/application/output/ManageBrandGatewayIntPort.java
@@ -1,8 +1,11 @@
 package com.bootcamp.microservicio_stock.application.output;
 
 import com.bootcamp.microservicio_stock.domain.models.Brand;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ManageBrandGatewayIntPort {
     public Brand save(Brand objBrand);
     public boolean existsBrandyByName(String name);
+    Page<Brand> findAllBrands(Pageable pageable);
 }
