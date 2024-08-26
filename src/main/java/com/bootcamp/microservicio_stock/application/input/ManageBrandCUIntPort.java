e9c79b4bc8e2a033c4e244064d9c033f61751fa4
FelipePerez99 <andresfppy@gmail.com>
2024-08-26 14:19:21 -0500

    feat: agregar metodo listBrands a ManageBrandCUIntPort

--- src/main/java/com/bootcamp/microservicio_stock/application/input/ManageBrandCUIntPort.java
+++ src/main/java/com/bootcamp/microservicio_stock/application/input/ManageBrandCUIntPort.java
@@ -1,7 +1,9 @@
 package com.bootcamp.microservicio_stock.application.input;
 
 import com.bootcamp.microservicio_stock.domain.models.Brand;
+import org.springframework.data.domain.Page;
 
 public interface ManageBrandCUIntPort {
     public Brand createBrand(Brand objBrand);
+    Page<Brand> listBrands(int page, int size, String sortBy, boolean ascending);
 }
